73b0c01156604ae14ff88f344b39b98ac98e56be
Shalev <pugiox@gmail.com>
2015-12-11 01:41:39 -0500

    First git_committer.

---
+++ git_commiter.rb
@@ -0,0 +1,29 @@
+def do_it
+  if modified_files.empty?
+    modified = git_modified_paths
+
+    return if modified.empty?
+
+    `git commit -a -m #{commit_message_from_modified(modified)}`
+  end
+end
+
+def git_modified_paths
+  `git status -s`.split("\n").map {|n| n.strip.split(/\s+/) }
+end
+
+def modified_files(root_path='.')
+  `find #{root_path} -not -path '*/\.*' -mtime -3s`
+end
+
+
+ACTIONS = Hash.new('changed').merge('M' => 'modified', '??' => 'added', 'D' => 'deleted')
+def commit_message_from_modified(modified_paths)
+  if note = modified_paths.find {|p| File.basename(p[1])[0] != '$'} || modified_paths.find {|p| File.basename(p[1])[0] == '$'}
+    "#{ACTIONS[note[0]].capitalize} #{File.basename(note[1])}"
+  elsif file = modified_paths.find {|p| p =~ /\.rb$/}
+    "Source Code: #{ACTIONS[file[0]].capitalize} #{File.basename(file[1])}"
+  end
+end
+
+do_it

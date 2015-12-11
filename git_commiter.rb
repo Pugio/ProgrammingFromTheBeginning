0fa00f4ccafb565867bb89d196557ea076553fc0
Shalev <pugiox@gmail.com>
2015-12-11 02:01:29 -0500

    Modified git_commiter.rb

--- git_commiter.rb
+++ git_commiter.rb
@@ -2,9 +2,9 @@ def commit_modified_files
   if modified_files.empty?
     modified = git_modified_paths
 
-    return if modified.empty?
+    return if modified.empty? || (modified.size == 1 && modified[0][1] =~ /\$__StoryList.tid/)
     p modified
-    `git commit -a -m "#{commit_message_from_modified(modified)}"`
+    `git add -A && git commit -a -m "#{commit_message_from_modified(modified)}"`
   end
 end
 
@@ -19,14 +19,13 @@ end
 
 ACTIONS = Hash.new('changed').merge('M' => 'modified', '??' => 'added', 'D' => 'deleted')
 def commit_message_from_modified(modified_paths)
-  if note = modified_paths.find {|p| File.basename(p[1])[0] != '$'} || modified_paths.find {|p| File.basename(p[1])[0] == '$'}
-    p note
-    puts note[1]
-    puts File.basename(note[1])
-    "#{ACTIONS[note[0]].capitalize} #{File.basename(note[1])}"
-  elsif file = modified_paths.find {|p| p =~ /\.rb$/}
-    "Source Code: #{ACTIONS[file[0]].capitalize} #{File.basename(file[1])}"
-  end
+  msg = if note = modified_paths.find {|p| File.basename(p[1])[0] != '$'} || modified_paths.find {|p| File.basename(p[1])[0] == '$'}
+          "#{ACTIONS[note[0]].capitalize} #{File.basename(note[1])}"
+        elsif file = modified_paths.find {|p| p =~ /\.rb$/}
+          "Source Code: #{ACTIONS[file[0]].capitalize} #{File.basename(file[1])}"
+        end
+
+  msg.gsub('$', '\$')
 end
 
 puts 'starting watcher'
